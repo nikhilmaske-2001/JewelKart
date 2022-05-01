8b84d00c2db1af86c8f1fdc7fed0cc8548ff1272
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-05-01 20:41:12 +0530

    Half work of footer is done.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,115 @@
+import { Facebook, Instagram, Pinterest, Twitter } from '@mui/icons-material';
+import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h1`
+
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+`;
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.div`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.div`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+
+`;
+
+const Footer = () => {
+  return (
+      <Container>
+          <Left>
+            <Logo>LAMA.</Logo>
+            <Desc>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            </Desc>
+            <SocialContainer>
+                <SocialIcon color="385999">
+                    <Facebook/>
+                </SocialIcon>
+                <SocialIcon color="E4405F">
+                    <Instagram/>
+                </SocialIcon>
+                <SocialIcon color="55ACEE">
+                    <Twitter/>
+                </SocialIcon>
+                <SocialIcon color="E60023">
+                    <Pinterest/>
+                </SocialIcon>
+            </SocialContainer>
+          </Left>
+          <Center>
+            <Title>Useful links</Title>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Man Fashion</ListItem>
+                <ListItem>Woman Fashion</ListItem>
+                <ListItem>Accessories</ListItem>
+                <ListItem>My Account</ListItem>
+                <ListItem>Order Tracking</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+            </List>
+          </Center>
+            <Right>
+                <Title>Contact</Title>
+                {/* <ContactItem>Mahal, Nagpur</ContactItem>
+                <ContactItem>+1 1234 2344</ContactItem>
+                <ContactItem>contact@nik.dev</ContactItem> */}
+            </Right>
+      </Container>
+  )
+}
+
+export default Footer
