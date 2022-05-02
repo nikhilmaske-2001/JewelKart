a221dc2298a7f87149181b25a5028d237f2f5178
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-05-02 18:18:55 +0530

    Shopping cart is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 
 const App = () => {
-  return(<Login/>);
+  return(<Cart/>);
 }
 export default App;
