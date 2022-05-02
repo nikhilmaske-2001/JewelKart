d71b38bdd47063663296f796e0be8eac181ec11b
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-05-02 17:29:16 +0530

    Login page is ready.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 
 const App = () => {
-  return(<Register/>);
+  return(<Login/>);
 }
 export default App;
