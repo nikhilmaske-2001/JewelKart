7982760f83562330a8995ee4fdf5227f3ea3e34f
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-05-05 17:11:32 +0530

    Created register api.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -12,7 +13,10 @@ mongoose
     .catch((err) => console.log(err));
 
 app.use(express.json())
+
 app.use("/api/users", userRoute);
+
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("Backend server is running");
