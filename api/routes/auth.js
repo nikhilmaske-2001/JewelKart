c9dab00c4d01c414793ad01c4479289ae59acc79
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-05-08 09:38:22 +0530

    Add api for update user.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // Register
 router.post("/register", async(req, res) => {
@@ -30,9 +31,16 @@ router.post("/login", async(req, res) => {
         Originalpassword !== req.body.password &&
         res.status(401).json("Wrong Credentials!");
 
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWT_SEC,
+        {expiresIn: "3d"}
+        );
+
         const {password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
 
     } catch (err) {
         res.status(500).json(err);
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,3 +1,21 @@
+const User = require("../models/User");
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
+
+// Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true})
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 module.exports = router;
