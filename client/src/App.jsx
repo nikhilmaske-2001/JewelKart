661f062c738c5f0ee674d1f1e403307aa64dae36
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-06-03 08:57:23 +0530

    Setup all routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,35 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import {
   BrowserRouter as Router,
-  Routes,
-  Route
+  Switch,
+  Route,
+  Redirect
 } from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return(
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/products/:category" element={<ProductList/>}/>
-        <Route path="/product/:id" element={<Product/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+        <Route path="/product/:id">
+          <Product/>
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route path="/login">
+          {user ? <Redirect to="/"/>: <Login/>}
+        </Route>
+        <Route path="/register">
+          {user ? <Redirect to="/"/>: <Register/>}
+        </Route>
+      </Switch>
     </Router>
   );
 }
