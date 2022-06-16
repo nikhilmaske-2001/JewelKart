9402d552913574aa6d29878ffe4fbf350af424b2
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-06-16 07:20:28 +0530

    Add link to the categories item component.

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import {mobile} from "../responsive";
 
@@ -48,11 +49,13 @@ const Button = styled.button`
 const CategoryItems = ({item}) => {
   return (
     <Container>
-      <Image src={item.img}/>
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
-      </Info>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img}/>
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+      </Link>
     </Container>
   )
 }
