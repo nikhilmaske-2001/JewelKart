c5043dccfa2b52df2770ba5935032c8dbaee9359
Nikhil Maske <nikhilmaske2001@gmail.com>
2022-06-29 20:27:15 +0530

    Using cors to fix the axios Network error.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
@@ -11,11 +12,12 @@ const orderRoute = require("./routes/order");
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("DB connection successfull"))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successfull"))
+  .catch((err) => console.log(err));
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use("/api/users", userRoute);
 
@@ -27,6 +29,6 @@ app.use("/api/cart", cartRoute);
 
 app.use("/api/order", orderRoute);
 
-app.listen(process.env.PORT || 5000, ()=> {
-    console.log("Backend server is running");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running");
 });
