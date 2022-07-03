f92cc04629d56b8f00b1f7846ee658ec07786d6f
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-07-03 11:45:13 +0530

    Added link of the product to searchOutlined

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,90 +1,96 @@
-import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
-import React from 'react';
-import styled from 'styled-components';
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Info = styled.div`
-    opacity: 0;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0,0,0,0.2);
-    z-index: 3;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: all 0.5s ease;
-    cursor: pointer;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
 `;
 
 const Container = styled.div`
-    flex: 1;
-    margin: 5px;
-    min-width: 280px;
-    height: 350px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #f5fbfd;
-    position: relative;
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
 
-    &:hover ${Info} {
-        opacity: 1;
-    }
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    background-color: white;
-    position: absolute;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
 `;
 
 const Image = styled.img`
-    height: 75%;
-    width: 75%;
-    z-index:2;
-    object-fit: cover;
+  height: 75%;
+  width: 75%;
+  z-index: 2;
+  object-fit: cover;
 `;
-
 
 const Icon = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    transition: all 0.5s ease;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
 
-    &:hover {
-        background-color: #e9f5f5;
-        transform: scale(1.1);
-    }
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
 `;
 
-const Product = ({item}) => {
+const Product = ({ item }) => {
   return (
     <Container>
-        <Circle/>
-        <Image src={item.img}/>
-        <Info>
-            <Icon>
-                <ShoppingCartOutlined/>
-            </Icon>
-            <Icon>
-                <SearchOutlined/>
-            </Icon>
-            <Icon>
-                <FavoriteBorderOutlined/>
-            </Icon>
-        </Info>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+        <Icon>
+          <Link to={`/product/${item._id}`}>
+            <SearchOutlined />
+          </Link>
+        </Icon>
+        <Icon>
+          <FavoriteBorderOutlined />
+        </Icon>
+      </Info>
     </Container>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
